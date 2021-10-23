ea139a634be52f0bdb01c1065191285124d804d8
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-23 23:48:00 +0900

    1 Store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
+
+// store - stores data, think of state
+// reducer - function that used to update store
+
+import { createStore } from 'redux';
+
+// reducer
+function reducer() {
+  console.log('hello world');
+}
+const store = createStore(reducer);
 
 function App() {
   // cart setup
