b5f1bce1f632269de0ea74982ecc3fd2cf71724b
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-26 23:52:27 +0900

    6 More Complicated State

--- src/App.js
+++ src/App.js
@@ -23,19 +23,24 @@ import { createStore } from 'redux';
 // initial store
 const initialStore = {
   count: 0,
+  name: 'john',
 };
 
 // reducer
 function reducer(state, action) {
   console.log({ state, action });
+
   if (action.type === 'DECREASE') {
-    return { count: state.count - 1 };
+    return { ...state, count: state.count - 1 };
   }
   if (action.type === 'INCREASE') {
-    return { count: state.count + 1 };
+    return { ...state, count: state.count + 1 };
   }
   if (action.type === 'RESET') {
-    return { count: 0 };
+    return { ...state, count: 0 };
+  }
+  if (action.type === 'CHANGE_NAME') {
+    return { ...state, name: 'bob' };
   }
 
   return state;
@@ -43,6 +48,7 @@ function reducer(state, action) {
 
 // store
 const store = createStore(reducer, initialStore);
+store.dispatch({ type: 'CHANGE_NAME' });
 store.dispatch({ type: 'DECREASE' });
 store.dispatch({ type: 'RESET' });
 store.dispatch({ type: 'INCREASE' });
