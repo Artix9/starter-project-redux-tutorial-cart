982773a204024b48c14aaf0273314960be88d113
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-27 22:09:55 +0900

    14 Switch Statement Syntax

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,18 @@
 import { DECREASE, INCREASE, CLEAR_CART } from './actions';
 
 function reducer(state, action) {
-  if (action.type === CLEAR_CART) {
-    return { ...state, cart: [] };
-  }
+  //   if (action.type === CLEAR_CART) {
+  //     return { ...state, cart: [] };
+  //   }
 
-  return state;
+  //   return state;
+
+  switch (action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: [] };
+    default:
+      return state;
+  }
 }
 
 export default reducer;
