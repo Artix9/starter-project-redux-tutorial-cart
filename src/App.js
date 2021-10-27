8b781680e0cdb13fa209380182a30bf2dd2b62bd
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-27 20:31:17 +0900

    8 Seperate Reducer

--- src/App.js
+++ src/App.js
@@ -4,42 +4,17 @@ import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 // items
 import cartItems from './cart-items';
+
 // redux stuff
-
-// store - stores data, think of state
-
-// reducer - function that used to update store
-// two arguments - state, action
-// state - old state / state before update
-// action - what happened / what update
-// return updated or old state
-
 import { createStore } from 'redux';
 import { DECREASE, INCREASE } from './actions';
-
-// dispatch method - send actions to the store
-// actions (objects) - MUST HAVE TYPE PROPERTY - what kind of action
-// DON'T MUTATE THE STATE - redux built on IMMUTABILITY (copy)
+import reducer from './reducer';
 
 // initial store
 const initialStore = {
   count: 0,
   name: 'john',
 };
-
-// reducer
-function reducer(state, action) {
-  console.log({ state, action });
-
-  if (action.type === DECREASE) {
-    return { ...state, count: state.count - 1 };
-  }
-  if (action.type === INCREASE) {
-    return { ...state, count: state.count + 1 };
-  }
-
-  return state;
-}
 
 // store
 const store = createStore(reducer, initialStore);
