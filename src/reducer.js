1325a7f5ebda19f3911421eece1c7ad2f66dd2cc
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-28 00:40:11 +0900

    20 DECREASE action complete

--- src/reducer.js
+++ src/reducer.js
@@ -5,8 +5,23 @@ function reducer(state, action) {
     return { ...state, cart: [] };
   }
   if (action.type === DECREASE) {
-    console.log('you decreased amount');
+    let tempCart = [];
+
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+    return { ...state, cart: tempCart };
   }
+
   if (action.type === INCREASE) {
     let tempCart = state.cart.map((cartItem) => {
       if (cartItem.id === action.payload.id) {
@@ -17,6 +32,7 @@ function reducer(state, action) {
     });
     return { ...state, cart: tempCart };
   }
+
   if (action.type === REMOVE) {
     return {
       ...state,
