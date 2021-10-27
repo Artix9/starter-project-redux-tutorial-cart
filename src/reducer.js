c20213acbb15bf9921ca6fa46def631947de4852
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-27 23:28:36 +0900

    15 Reducer Setup

--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,27 @@
-import { DECREASE, INCREASE, CLEAR_CART } from './actions';
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from './actions';
 
 function reducer(state, action) {
-  //   if (action.type === CLEAR_CART) {
-  //     return { ...state, cart: [] };
-  //   }
-
-  //   return state;
-
-  switch (action.type) {
-    case CLEAR_CART:
-      return { ...state, cart: [] };
-    default:
-      return state;
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
   }
+  if (action.type === DECREASE) {
+    console.log('you decreased amount');
+  }
+  if (action.type === INCREASE) {
+    console.log('you increased amount');
+  }
+  if (action.type === REMOVE) {
+    console.log('you removed amount');
+  }
+
+  return state;
 }
 
 export default reducer;
+
+//   switch (action.type) {
+//     case CLEAR_CART:
+//       return { ...state, cart: [] };
+//     default:
+//       return state;
+//   }
