675b6d4959fe9a1ccd159fda54c4ecb7207b9bfe
Artix9 <xxxxx01xxxxx@naver.com>
2021-10-30 23:42:22 +0900

    23 TOGGLE_AMOUNT action

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,11 @@
-import { DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTALS } from './actions';
+import {
+  DECREASE,
+  INCREASE,
+  CLEAR_CART,
+  REMOVE,
+  GET_TOTALS,
+  TOGGLE_AMOUNT,
+} from './actions';
 
 function reducer(state, action) {
   if (action.type === CLEAR_CART) {
@@ -49,6 +56,23 @@ function reducer(state, action) {
     );
     total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
+  }
+
+  if (action.type === TOGGLE_AMOUNT) {
+    return {
+      ...state,
+      cart: state.cart.map((cartItem) => {
+        if (action.payload.id === cartItem.id) {
+          if (action.payload.toggle === 'inc') {
+            return (cartItem = { ...cartItem, amount: cartItem.amount + 1 });
+          }
+          if (action.payload.toggle === 'dec') {
+            return (cartItem = { ...cartItem, amount: cartItem.amount - 1 });
+          }
+        }
+        return cartItem;
+      }),
+    };
   }
 
   return state;
