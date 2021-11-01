66f35ae366dabfefcfcbe505b287da695f3fe77f
Artix9 <xxxxx01xxxxx@naver.com>
2021-11-01 23:52:58 +0900

    26 Remove comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-// components
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
-
-// redux stuff
 import { createStore } from 'redux';
 import reducer from './reducer';
-
-// react-redux - Provider - wraps app, connect - used in components
 import { Provider } from 'react-redux';
 
-// store
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 function App() {
-  // cart setup
-
   return (
     <Provider store={store}>
       <Navbar />
